b5ed20e2ac306c00197bf76a51c8400571adde29
vsemoth <vsemoth@gmail.com>
2021-07-23 21:59:06 +0200

    Netlify Config

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,22 +1,18 @@
-import Head from "next/head"
+import Head from 'next/head'
 import Layout, { siteTitle } from '../../components/layout'
 import utilStyles from '../../styles/utils.module.css'
 
-const Admin = () => {
+export default function Home({ allPostsData }) {
     return (
-        <Layout>
+      <Layout>
         <Head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Content Manager</title>
-            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Content Manager</title>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
             <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
-        </section>
-
-        </Layout>
-    );
-}
- 
-export default Admin;
+      </section>
+    </Layout>
+    )}
